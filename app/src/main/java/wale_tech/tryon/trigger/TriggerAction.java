3544d9e3d565bb42d202c2e572e7a275d60816a3
CHwithKeY <Hugh_Lambert@outlook.com>
2016-12-10 22:10:35 +0800

    Fix the bug ---  When the TriggerService.java call the TriggerAct.java, add progressbar to refresh UI and handle the UI request.

--- app/src/main/java/wale_tech/tryon/trigger/TriggerAction.java
+++ app/src/main/java/wale_tech/tryon/trigger/TriggerAction.java
@@ -1,6 +1,10 @@
 package wale_tech.tryon.trigger;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +19,8 @@ import wale_tech.tryon.http.HttpAction;
 import wale_tech.tryon.http.HttpHandler;
 import wale_tech.tryon.http.HttpSet;
 import wale_tech.tryon.http.HttpTag;
+import wale_tech.tryon.login.Login_Act;
+import wale_tech.tryon.product.ProductAction;
 import wale_tech.tryon.publicObject.ObjectShoe;
 
 /**
@@ -25,16 +31,32 @@ public class TriggerAction extends BaseAction {
 
     private Base_Serv base_serv;
 
+    private ProgressBar progressBar;
+
+    private ProductAction productAction;
+
     public TriggerAction(Context context) {
         super(context);
+        viewInit();
     }
 
     public TriggerAction(Context context, Base_Serv base_serv) {
         super(context);
         this.base_serv = base_serv;
+        viewInit();
     }
 
-    public int getList(int request, String trigger_result, String trigger_path, String trigger_work_space, boolean isShowDialog) {
+    private void viewInit() {
+        if (base_serv == null) {
+            if (progressBar == null) {
+                progressBar = (ProgressBar) ((AppCompatActivity) context).findViewById(R.id.trigger_pb);
+            }
+
+            productAction = new ProductAction(context);
+        }
+    }
+
+    public int getList(int request, String trigger_result, String trigger_path, String trigger_work_space, boolean isShowDialog, boolean isShowProgressBar) {
         if (!checkNet()) {
             return ACTION_NET_DOWN;
         }
@@ -57,6 +79,8 @@ public class TriggerAction extends BaseAction {
             action.setDialog(context.getString(R.string.base_search_progress_title), context.getString(R.string.base_search_progress_msg));
         }
 
+        setProgressBar(isShowProgressBar);
+
         if (base_serv == null) {
             action.setHandler(new HttpHandler(context));
         } else {
@@ -65,6 +89,10 @@ public class TriggerAction extends BaseAction {
 
         action.interaction();
         return ACTION_DONE;
+    }
+
+    public void onGetCoupon() {
+        productAction.onGetCoupon();
     }
 
     public ArrayList<ObjectShoe> handleListResponse(String result) throws JSONException {
@@ -92,6 +120,26 @@ public class TriggerAction extends BaseAction {
         }
 
         return shoeList;
+    }
+
+    public void handleCouponAwardResponse(String result) throws JSONException {
+        productAction.handleCouponAwardResponse(result);
+    }
+
+    public void setProgressBar(boolean isShow) {
+        if (progressBar != null) {
+            if (isShow) {
+                progressBar.setVisibility(View.VISIBLE);
+            } else {
+                progressBar.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    public void cancelProgressBar() {
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
 }
