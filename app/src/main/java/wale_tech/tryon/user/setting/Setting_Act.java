f7bce5588a1c9d42c0c09717d8c7a12d7eb20821
WaleTech <rfid@wale-tech.com>
2017-04-05 15:11:11 +0800

    Feature Developed:      -.Add a new item in Setting_Act.java, named "Set Dedicated Net Manually", so that the user can enter the IP through it, instead of modifying the source code.      Besides, also add some parameter in SharedSet.java and strings.xml to make it work.

--- app/src/main/java/wale_tech/tryon/user/setting/Setting_Act.java
+++ app/src/main/java/wale_tech/tryon/user/setting/Setting_Act.java
@@ -38,6 +38,8 @@ public class Setting_Act extends Base_Act implements View.OnClickListener {
 
         setupGetRefreshTimeText();
 
+        setupDedicatedNetManuallyText();
+
         setupSetDedicatedNetText();
 
         setupCheckNetText();
@@ -74,9 +76,14 @@ public class Setting_Act extends Base_Act implements View.OnClickListener {
         get_time_tv.setOnClickListener(this);
     }
 
+    private void setupDedicatedNetManuallyText() {
+        final TextView dedicated_net_manual_tv = (TextView) findViewById(R.id.setting_set_dedicated_net_manual_tv);
+        dedicated_net_manual_tv.setOnClickListener(this);
+    }
+
     private void setupSetDedicatedNetText() {
-        final TextView check_net_tv = (TextView) findViewById(R.id.setting_set_dedicated_net_tv);
-        check_net_tv.setOnClickListener(this);
+        final TextView dedicated_net_tv = (TextView) findViewById(R.id.setting_set_dedicated_net_tv);
+        dedicated_net_tv.setOnClickListener(this);
     }
 
     private void setupCheckNetText() {
@@ -123,6 +130,10 @@ public class Setting_Act extends Base_Act implements View.OnClickListener {
 
             case R.id.setting_get_refresh_time_tv:
                 onShowRefreshTime();
+                break;
+
+            case R.id.setting_set_dedicated_net_manual_tv:
+                onSetDedicatedNetManually();
                 break;
 
             case R.id.setting_set_dedicated_net_tv:
@@ -177,13 +188,58 @@ public class Setting_Act extends Base_Act implements View.OnClickListener {
                 .show();
     }
 
+    private void onSetDedicatedNetManually() {
+        final View dedicated_net_edit_view = View.inflate(this, R.layout.dialog_edit_dedicated_net_view, null);
+
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.setting_dialog_set_dedicated_net_manually_title))
+                .setView(dedicated_net_edit_view)
+                .setPositiveButton(getString(R.string.base_dialog_btn_confirm), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText net_et = (EditText) dedicated_net_edit_view.findViewById(R.id.dialog_ert_time_et);
+                        String net_str = net_et.getText().toString();
+                        if (net_str.isEmpty()) {
+                            showSnack(0, getString(R.string.setting_snack_dedicated_net_cannot_null));
+                        } else {
+                            sharedAction.setDedicatedIP(net_str);
+//                                sharedAction.setRefreshTime(time);
+                            showSnack(0, getString(R.string.setting_snack_set_success));
+                        }
+                    }
+                })
+                .setNegativeButton(getString(R.string.base_dialog_btn_cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .show();
+    }
+
     private void onSetDedicatedNet() {
         String[] net_list = {
                 getString(R.string.setting_dialog_gzd_net),
-                getString(R.string.setting_dialog_bs_net)
+                getString(R.string.setting_dialog_bs_net),
+                getString(R.string.setting_dialog_other_net)
         };
 
-        int net_default = HttpSet.DEDICATED_IP.equals(HttpSet.GZD_DEDICATED_IP) ? 0 : 1;
+        int net_default = 2;
+        switch (HttpSet.DEDICATED_IP) {
+            case HttpSet.GZD_DEDICATED_IP:
+                net_default = 0;
+                break;
+
+            case HttpSet.BS_DEDICATED_IP:
+                net_default = 1;
+                break;
+
+            default:
+                net_default = 2;
+                break;
+        }
+
+//        int net_default = HttpSet.DEDICATED_IP.equals(HttpSet.GZD_DEDICATED_IP) ? 0 : 1;
 
         new AlertDialog.Builder(this)
                 .setTitle(getString(R.string.setting_dialog_choose_dedicated_net_title))
@@ -197,6 +253,10 @@ public class Setting_Act extends Base_Act implements View.OnClickListener {
 
                             case 1:
                                 HttpSet.setDedicatedIp(HttpSet.BS_DEDICATED_IP);
+                                break;
+
+                            case 2:
+                                HttpSet.setDedicatedIp(sharedAction.getDedicatedIP());
                                 break;
                         }
                         dialog.dismiss();
